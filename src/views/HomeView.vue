b69ff7466186b6612e8565458278cfc52b934db6
Laila <12150120132@students.uin-suska.ac.id>
2023-05-16 20:14:40 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+    <div class="home">
+      <img src="../assets/masjid.jpg" >
+    </div>
+  </template>
+  
+  <style>
+  img {
+    margin-left: auto;
+    margin-right: auto;
+    padding-top: 10px;
+    width:1350px;
+    height:500px;
+  }
+  
+  </style>
